991ae356de534253e9e07ea8aef4126d0db0de84
ErkanGok <43829164+ErkanGok@users.noreply.github.com>
2021-08-17 21:00:45 +0300

    Address refactor

--- YemekSepetiClone/Models/User/User.cs
+++ YemekSepetiClone/Models/User/User.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public Address Address { get; set; } = new();
+        public string Address { get; set; } 
         public string Phone { get; set; }
     }
 }
